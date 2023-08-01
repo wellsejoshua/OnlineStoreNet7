a12813a9a68c81d97ad9f4a07c02a04b5e356b2c
Joshua Wells <codedbywells@gmail.com>
2023-08-01 00:07:21 -0500

    Add project files.

---
+++ OnlineStoreFrontNet7/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreFrontNet7.Models;
+using System.Diagnostics;
+
+namespace OnlineStoreFrontNet7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
